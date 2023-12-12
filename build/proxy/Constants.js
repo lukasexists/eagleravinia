1417667dda6d8913bc5abff68366b6436f65a64c
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-12-12 11:33:26 -0500

    Update page

--- build/proxy/Constants.js
+++ build/proxy/Constants.js
@@ -12,4 +12,27 @@ export var Constants;
     Constants.JOIN_SERVER_PACKET = 0x01;
     Constants.PLAYER_LOOK_PACKET = 0x08;
 })(Constants || (Constants = {}));
-export const UPGRADE_REQUIRED_RESPONSE = `<!DOCTYPE html><!-- Served by ${meta.PROXY_BRANDING} (version: ${meta.PROXY_VERSION}) --><html> <head> <title>Eagleravinia Proxy</title> <style> :root { font-family: "Arial" } code { padding: 3px 10px 3px 10px; border-radius: 5px; font-family: monospace; background-color: #1a1a1a; color: white; } </style> <script type="text/javascript"> window.addEventListener('load', () => { document.getElementById("connect-url").innerHTML = window.location.href.replace(window.location.protocol, window.location.protocol == "https:" ? "wss:" : "ws:"); }); </script> </head> <body> <h1>Eagleravinia Proxy</h1> <p>It appears as if you've reached the landing page for this EaglerProxy instance. To connect, use this server IP/URL: <code id="connect-url">loading...</code> (connect from any 1.8 EaglercraftX client via Multiplayer > Direct Connect)</p> </body></html>`;
+export const UPGRADE_REQUIRED_RESPONSE = `<!DOCTYPE html>
+<!-- Served by ${meta.PROXY_BRANDING} (version: ${meta.PROXY_VERSION}) -->
+<html> 
+<head> 
+<title>Deadly</title> 
+<style> 
+:root { font-family: "Arial" } 
+body { background-color: #000; color: #eee; }
+code { padding: 3px 10px 3px 10px; border-radius: 5px; font-family: monospace; background-color: #1a1a1a; color: white; } 
+</style> 
+
+<script type="text/javascript"> 
+window.addEventListener('load', () => { 
+  document.getElementById("connect-url").innerHTML = window.location.href.replace(window.location.protocol, window.location.protocol == "https:" ? "wss:" : "ws:"); 
+}); 
+</script> 
+</head> 
+
+<body> 
+<h1>Deadly</h1> 
+<p>Welcome to the Deadly home page! To connect, use this server IP/URL: <code id="connect-url">loading...</code> (connect from any 1.8 EaglercraftX client via Multiplayer > Direct Connect!)</p> 
+<p>Ravinia SMP members must message an admin to enter the SMP world.</p>
+</body>
+</html>`;
