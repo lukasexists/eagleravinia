9d9d727b907f0400d5cd06c31b69adbc26459a58
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-12-13 21:43:46 +0000

    HTML reformatted

--- build/proxy/Constants.js
+++ build/proxy/Constants.js
@@ -35,14 +35,18 @@ window.addEventListener('load', () => {
 
 <body> 
 <div class="content">
+
 <h1>Hazard&shy;craft</h1> 
+
 <p>
 Hazardcraft is a 1.8.9 Eaglercraft SMP that takes place on the highly-hazardous C-814 planet.
 The sun is hazardous to humans, and the animals are highly aggressive hunters.
 </p> 
 <h2>Play at <code>wss://hazard.owouw.us/</code></h2>
 <p>Ravinia SMP members must message an admin to enter the SMP world.</p>
+
 <h2>Addon List</h2>
+
 <ul>
 <li>AuthMe obviously</li>
 <li>Quality Armory</li>
@@ -56,6 +60,7 @@ The sun is hazardous to humans, and the animals are highly aggressive hunters.
 <li>DeathCoordinates</li>
 <li>Multiworld</li>
 </ul>
+
 </div>
 </body>
 </html>`;
